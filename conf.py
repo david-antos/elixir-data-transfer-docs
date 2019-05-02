885bb7b0e5cb1890c9485cac69e4ebd5c35a22e3
Mikael Borg <mikael.borg@nbis.se>
2019-05-02 15:13:49 +0200

    Use readthedocs theme. Use Elixir logo.

--- conf.py
+++ conf.py
@@ -83,19 +83,13 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import os
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
-if on_rtd:
-    html_theme = 'default'
-else:
-    html_theme = 'classic'
-
+html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 # html_theme_options = {}
-
+html_logo = 'images/elixir-logo.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
